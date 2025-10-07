871ad04e43849d843e85ca85ec859bac1a8f5884
yoyo326 <yoyoguinness@gmail.com>
2025-10-07 14:00:19 +0900

    admin/companies 관리자- 업체관리 페이지 프론트

---
+++ src/main/java/com/example/ApiRound/crm/yoyo/AdminManageCompanyController.java
@@ -0,0 +1,24 @@
+package com.example.ApiRound.crm.yoyo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminManageCompanyController {
+
+    @GetMapping("/companies")
+    public String companies(Model model) {
+        // 업체 관리 페이지 데이터 설정
+        model.addAttribute("totalCompanies", 326);
+        model.addAttribute("newThisMonth", 13);
+        model.addAttribute("reportsReceived", 6);
+        model.addAttribute("underSanction", 2);
+        
+        return "crm/admin_manage_company";
+    }
+
+
+}
