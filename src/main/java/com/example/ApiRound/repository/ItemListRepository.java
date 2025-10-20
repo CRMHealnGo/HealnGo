70b5bcd626ee0dffda1a64060b6607dd12f60a26
mingzz <hanyu0235@gmail.com>
2025-10-20 18:00:56 +0900

    메인화면

--- src/main/java/com/example/ApiRound/repository/ItemListRepository.java
+++ src/main/java/com/example/ApiRound/repository/ItemListRepository.java
@@ -1,15 +1,18 @@
 package com.example.ApiRound.repository;
 
-import com.example.ApiRound.entity.ItemList;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.Optional;
+import com.example.ApiRound.entity.ItemList;
 
 @Repository
 public interface ItemListRepository extends JpaRepository<ItemList, Long> {
